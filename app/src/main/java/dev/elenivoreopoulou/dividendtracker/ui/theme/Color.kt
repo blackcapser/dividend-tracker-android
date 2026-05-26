7a66d2d752a9511b2865ac387eb245dea407a38c
elenivoreopoulou <eleni.voreopoulou@alpha.gr>
2026-05-26 11:39:25 +0300

    Implement Figma color system and typography ffoundations

--- app/src/main/java/dev/elenivoreopoulou/dividendtracker/ui/theme/Color.kt
+++ app/src/main/java/dev/elenivoreopoulou/dividendtracker/ui/theme/Color.kt
@@ -2,10 +2,39 @@ package dev.elenivoreopoulou.dividendtracker.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Primary
+val PrimaryBlue = Color(0xFF8AA0FF)
+val PrimaryBlueDark = Color(0xFF5F6FEF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Secondary
+val SuccessGreen = Color(0xFF34D399)
+val PremiumGold = Color(0xFFC7A86B)
+
+// Dark Theme
+val DarkBackground = Color(0xFF0B0D14)
+val DarkSurface = Color(0xFF121826)
+val DarkSurfaceSecondary = Color(0xFF1A2133)
+val DarkSurfaceElevated = Color(0xFF222C42)
+val DarkOutline = Color(0xFF2E3A55)
+
+// Light Theme
+val LightBackground = Color(0xFFF6F7FB)
+val LightSurface = Color(0xFFFFFFFF)
+val LightSurfaceSecondary = Color(0xFFEEF1F8)
+val LightSurfaceElevated = Color(0xFFE4E9F3)
+val LightOutline = Color(0xFFD3DBEA)
+
+// Text Dark
+val DarkTextPrimary = Color(0xFFF4F6FF)
+val DarkTextSecondary = Color(0xFFA9B2C7)
+val DarkTextMuted = Color(0xFF6E7891)
+
+// Text Light
+val LightTextPrimary = Color(0xFF0F172A)
+val LightTextSecondary = Color(0xFF49546E)
+val LightTextMuted = Color(0xFF7B849D)
+
+// Semantic
+val ErrorRed = Color(0xFFFF7070)
+val WarningYellow = Color(0xFFFBBF24)
+val InfoBlue = Color(0xFF8AA0FF)
